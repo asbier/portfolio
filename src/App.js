a9982a816892b8cc2a2c6273f3957791d565297b
asbier-lab <annemariesauerbier@gmail.com>
2025-01-07 13:01:34 +0100

    changed App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,39 @@
 //import logo from './logo.svg';
 import React from "react";
-import Nav from "./components/Nav"
-import Footer from "./components/Footer"
-import Hero from "./components/Hero"
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import Main from "./components/Main";
 import './App.css';
 import About from "./pages/About";
 import Privacy from "./pages/Privacy";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
-import {Route,Routes } from 'react-router-dom';
-
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-
-  const [darkMode, setDarkMode] = React.useState(false)
-  
+  const [darkMode, setDarkMode] = React.useState(false);
 
   function toggleDarkMode() {
-    setDarkMode(prevMode => !prevMode)
-  
-}
+    setDarkMode((prevMode) => !prevMode);
+  }
 
   return (
-
-<div ClassName="body-dark" >
-
-
-  <Nav darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
-  <div className="container-page"> 
- <Routes> <Route path='/' element={<Home/>}/>
- <Route path='/About' element={<About/>}/> 
- <Route path='/Privacy' element={<Privacy/>}/>
- <Route path='/Contact' element={<Contact/>}/>
-
-  </Routes>
-  </div>
-
-
-
-  <Hero darkMode={darkMode} />
-  <Main  darkMode={darkMode}/>
-  <Footer darkMode={darkMode}/>
-</div>
-);
+    <div className={darkMode ? "body-dark" : "body-light"}>
+      <Nav darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <div className="container-page"> 
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Privacy' element={<Privacy />} />
+          <Route path='/Contact' element={<Contact />} />
+        </Routes>
+      </div>
+      <Hero darkMode={darkMode} />
+      <Main darkMode={darkMode} />
+      <Footer darkMode={darkMode} />
+    </div>
+  );
 }
 
-
-  export default App;
+export default App;
