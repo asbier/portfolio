25d14ac9ee4ae4e87d9a78e5ad8b0c8530a0a43f
JimmyNxn <141742096+JimmyNxn@users.noreply.github.com>
2025-01-13 21:30:35 +0100

    feat: adjust mobile setting

--- src/components/Main.js
+++ src/components/Main.js
@@ -84,6 +84,8 @@ function Main(props) {
         <motion.div
           drag="x"
           dragConstraints={{ right: 0, left: -width }}
+          dragElastic={0.1} /* Reduce elastic feel for more natural drag */
+          dragMomentum={false}
           className="inner-carousel"
           onDragStart={handleDragStart}
           onDragEnd={handleDragEnd}
