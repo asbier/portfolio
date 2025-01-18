a77e2a79e8e5e1cea71f5f9836cc85fde82ef26d
JimmyNxn <141742096+JimmyNxn@users.noreply.github.com>
2025-01-18 14:17:48 +0100

    feat: stick modal close button to top right corner

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import './Modal.css';
+
 const Modal = ({ isOpen, onClose, content }) => {
     if (!isOpen) return null;
 
     return (
         <div className="modal">
             <div className="modal-content">
-                <span className="close" onClick={onClose}>&times;</span>
+                <button className="close" onClick={onClose}>&times;</button>
                 {content}
             </div>
         </div>
