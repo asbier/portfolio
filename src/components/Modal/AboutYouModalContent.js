1c7f1199f32108b43585b18c64fb33dab802aff8
JimmyNxn <141742096+JimmyNxn@users.noreply.github.com>
2025-01-21 18:21:01 +0100

    fix: broken path in AboutYouModalContent.js

--- src/components/Modal/AboutYouModalContent.js
+++ src/components/Modal/AboutYouModalContent.js
@@ -1,50 +1,42 @@
 import React from 'react';
 import './AboutYouModalContent.css';
 
-const EditedModalContent = () => (
+// Ensure the component name matches the export name
+const AboutYouModalContent = () => (
     <div className="aboutyou-modal-content-container">
-        <h2> AboutYou</h2>
-
+        <h2> About You</h2>
         <p>
-<h3>At About You</h3>
-<p>
-    I played a significant role in the creative evolution of the company’s brand strategy, contributing to key projects like the LeGer by Lena Gercke label and other major brand initiatives. During my time there, About You was undergoing a strategic shift towards a broader, mainstream audience, which required adapting lessons learned from previous projects in more niche markets. This process focused on enhancing personalization and creating a user-centered platform, key to competing with major players in the e-commerce sector.
-</p>
-
-<h3>Key Contributions</h3>
-
-<section className="modal-section">
-<div className="aboutyou-section">
-    <h4>Recreating Brand Strategy</h4>
-    <p>Collaborated with cross-functional teams to redefine About You’s approach, focusing on enhancing user engagement through personalization and ensuring the platform spoke to a more diverse and mainstream audience.</p>
-    <p>Helped develop concepts that positioned the platform as a celebration of individuality, emphasizing the user as the core of the experience, rather than just fashion itself.</p>
-    <p>Incorporated market trends from high-performing regions to align with industry shifts, ensuring that About You could capture a larger market share.</p>
-
-    <h4>Creative Freedom & Experimentation</h4>
-    <p>Enjoyed the creative freedom to explore innovative ideas, fostering a culture of experimentation. This approach allowed us to test concepts and refine them based on real-time feedback, ensuring the ideas that resonated most with users were brought to life.</p>
-    <p>Embraced a trial-and-error mindset, which helped the brand evolve organically while maintaining flexibility and agility in execution.</p>
-
-    <h4>Website and Commercial Adjustments</h4>
-    <p>Contributed to the development of a new website structure focused on enhancing the sense of individuality and personal connection with the audience.</p>
-    <p>Worked on conceptualizing campaigns and commercials that resonated with a wider, more diverse audience, emphasizing inclusivity and user-driven content.</p>
-
-    <h4>Influencer and Celebrity Engagement</h4>
-    <p>Developed the “Celebrity X” initiative to incorporate well-known public figures into campaigns, making the brand feel more relatable and authentic, and directly engaging with a broader user base.</p>
-
-    <h4>Store & Event Concepts</h4>
-    <p>Designed innovative store concepts and pop-up events to enhance the brand’s physical presence and improve consumer interaction with the platform.</p>
-    <p>Developed branding strategies for high-profile events that aligned with the company’s vision of creating an engaging and immersive shopping experience.</p>
-
-    <h4>Collaboration with External Brands</h4>
-    <p>Led efforts to collaborate with well-known external designers and influencers, creating strategic partnerships that elevated the brand’s visibility and market positioning.</p>
-    <p>Worked on co-branded campaigns that aligned the About You aesthetic with well-established names in fashion, making the platform more appealing to a larger audience.</p>
-
-    <h4>Strategic Adaptation</h4>
-    <p>Contributed to the shift in strategy as About You transitioned from a niche e-commerce platform to a more mainstream approach, focusing on diversity and inclusivity, with an emphasis on personalization.</p>
-    <p>Ensured that the platform’s structure, content, and branding strategies aligned with these shifts, fostering a deeper connection with a broader customer base.</p>
-</div>
-</section>
-</p>
+            <h3>At About You</h3>
+            <p>
+                I played a significant role in the creative evolution of the company’s brand strategy, contributing to key projects like the LeGer by Lena Gercke label and other major brand initiatives. During my time there, About You was undergoing a strategic shift towards a broader, mainstream audience, which required adapting lessons learned from previous projects in more niche markets. This process focused on enhancing personalization and creating a user-centered platform, key to competing with major players in the e-commerce sector.
+            </p>
+            <h3>Key Contributions</h3>
+            <section className="modal-section">
+                <div className="aboutyou-section">
+                    <h4>Recreating Brand Strategy</h4>
+                    <p>Collaborated with cross-functional teams to redefine About You’s approach, focusing on enhancing user engagement through personalization and ensuring the platform spoke to a more diverse and mainstream audience.</p>
+                    <p>Helped develop concepts that positioned the platform as a celebration of individuality, emphasizing the user as the core of the experience, rather than just fashion itself.</p>
+                    <p>Incorporated market trends from high-performing regions to align with industry shifts, ensuring that About You could capture a larger market share.</p>
+                    <h4>Creative Freedom & Experimentation</h4>
+                    <p>Enjoyed the creative freedom to explore innovative ideas, fostering a culture of experimentation. This approach allowed us to test concepts and refine them based on real-time feedback, ensuring the ideas that resonated most with users were brought to life.</p>
+                    <p>Embraced a trial-and-error mindset, which helped the brand evolve organically while maintaining flexibility and agility in execution.</p>
+                    <h4>Website and Commercial Adjustments</h4>
+                    <p>Contributed to the development of a new website structure focused on enhancing the sense of individuality and personal connection with the audience.</p>
+                    <p>Worked on conceptualizing campaigns and commercials that resonated with a wider, more diverse audience, emphasizing inclusivity and user-driven content.</p>
+                    <h4>Influencer and Celebrity Engagement</h4>
+                    <p>Developed the “Celebrity X” initiative to incorporate well-known public figures into campaigns, making the brand feel more relatable and authentic, and directly engaging with a broader user base.</p>
+                    <h4>Store & Event Concepts</h4>
+                    <p>Designed innovative store concepts and pop-up events to enhance the brand’s physical presence and improve consumer interaction with the platform.</p>
+                    <p>Developed branding strategies for high-profile events that aligned with the company’s vision of creating an engaging and immersive shopping experience.</p>
+                    <h4>Collaboration with External Brands</h4>
+                    <p>Led efforts to collaborate with well-known external designers and influencers, creating strategic partnerships that elevated the brand’s visibility and market positioning.</p>
+                    <p>Worked on co-branded campaigns that aligned the About You aesthetic with well-established names in fashion, making the platform more appealing to a larger audience.</p>
+                    <h4>Strategic Adaptation</h4>
+                    <p>Contributed to the shift in strategy as About You transitioned from a niche e-commerce platform to a more mainstream approach, focusing on diversity and inclusivity, with an emphasis on personalization.</p>
+                    <p>Ensured that the platform’s structure, content, and branding strategies aligned with these shifts, fostering a deeper connection with a broader customer base.</p>
+                </div>
+            </section>
+        </p>
     </div>
 );
 
