ece25ea5f722ac587954ab154967f8c367c17ebe
JimmyNxn <141742096+JimmyNxn@users.noreply.github.com>
2025-01-26 12:50:27 +0100

    feat: passwordpage

---
+++ src/components/PasswordPage/PasswordPage.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './PasswordPage.css';
+
+const PasswordPage = ({ onPasswordCorrect }) => {
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const correctPassword = process.env.REACT_APP_PASSWORD; // Use environment variable
+
+    if (password === correctPassword) {
+      onPasswordCorrect();
+    } else {
+      setError('Incorrect password. Please try again.');
+      setPassword(''); // Clear the password input field
+    }
+  };
+
+  return (
+    <div className="password-page">
+      <h1>Annemarie's Portfolio</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="Enter password"
+        />
+        <button type="submit">Enter</button>
+      </form>
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
+};
+
+export default PasswordPage;
---
+++ src/components/PasswordProtect/PasswordProtect.js
@@ -0,0 +1,19 @@
+// src/components/PasswordProtect/PasswordProtect.js
+import React, { useState } from 'react';
+import PasswordPage from '../PasswordPage/PasswordPage';
+
+const PasswordProtect = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handlePasswordCorrect = () => {
+    setIsAuthenticated(true);
+  };
+
+  if (!isAuthenticated) {
+    return <PasswordPage onPasswordCorrect={handlePasswordCorrect} />;
+  }
+
+  return <>{children}</>;
+};
+
+export default PasswordProtect;
